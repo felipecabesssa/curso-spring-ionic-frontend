69be36183278cbc8519c6058fb4673eccc221894
felipecabesssa <felipesoares100@hotmail.com>
2022-02-06 13:20:27 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -15,15 +16,27 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams, 
-    public ProdutoService: ProdutoService) {
+    public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get('categoria_id');
-    this.ProdutoService.findByCategoria( categoria_id )
+    this.produtoService.findByCategoria( categoria_id )
       .subscribe(response => {
         this.items = response['content'];
+        this.loadImageUrls();
       },
       error => {});
+  }
+
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {});
+    }
   }
 }
